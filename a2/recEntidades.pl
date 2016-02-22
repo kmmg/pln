1017cd01bd1fb39b8185851ef2e2035b9c13c8ac
Carlos <karlosmmg@gmail.com>
2016-02-22 10:23:07 +0000

    update

--- a2/recEntidades.pl
+++ a2/recEntidades.pl
@@ -22,10 +22,11 @@ while (<>){
 #para ordenar pelo número de ocorrências(valor) temos de passar o operador (exemplo: $palavras{$a} <=> $palavras{$b})
 #foreach my $p (sort($palavras{$a} <=> $palavras{$b}) keys %palavras){
 #podemos utilizar também uma função de comparação que nos sirva
-foreach my $p (sort( mycompare($a,$b) ) keys %palavras){
+foreach my $p (sort {mycompare($a,$b)} keys %palavras){
 	print "$p: $palavras{$p}\n";
 }
 
+#ordena por número de ocorrências inverso e dentro delas alphabeticamente
 sub mycompare{my($a,$b)=@_;
 	if($palavras{$a} < $palavras{$b} {return 1});
 	if($palavras{$a} > $palavras{$b} {return -1});
